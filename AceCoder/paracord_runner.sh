83838e72fa5b003c1a97a612e0cc25145fa92612
SWESH1K <136169450+SWESH1K@users.noreply.github.com>
2024-09-06 09:43:28 +0530

    try4

--- AceCoder/paracord_runner.sh
+++ AceCoder/paracord_runner.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source /opt/venv/bin/activate
-python manage.py migrate
+python manage.py migrate --no-input
 python manage.py collectstatic --noinput
 gunicorn AceCoder.wsgi:application --bind 0.0.0.0:8000
